0dca0d37425c3782dfde53102de74c7453fddc80
yaeby <adrian.copta@isa.utm.md>
2024-02-24 00:52:01 +0200

    working on laba 2, still...

--- Lab_2/sorting.py
+++ Lab_2/sorting.py
@@ -1,22 +1,6 @@
 from random import randint
 from timeit import repeat
 
-def run_sorting_algorithm(algorithm, array):
-    # Set up the context and prepare the call to the specified
-    # algorithm using the supplied array. Only import the
-    # algorithm function if it's not the built-in `sorted()`.
-    setup_code = f"from __main__ import {algorithm}" \
-        if algorithm != "sorted" else ""
-
-    stmt = f"{algorithm}({array})"
-
-    # Execute the code ten different times and return the time
-    # in seconds that each execution took
-    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
-
-    # Finally, display the name of the algorithm and the
-    # minimum time it took to run
-    print(f"Algorithm: {algorithm}. Minimum execution time: {min(times)}")
 
 def bubble_sort(array):
     n = len(array)
@@ -79,14 +63,168 @@ def insertion_sort(array):
 
     return array
 
-ARRAY_LENGTH = 10000
+def merge(left, right):
+    # If the first array is empty, then nothing needs
+    # to be merged, and you can return the second array as the result
+    if len(left) == 0:
+        return right
+
+    # If the second array is empty, then nothing needs
+    # to be merged, and you can return the first array as the result
+    if len(right) == 0:
+        return left
+
+    result = []
+    index_left = index_right = 0
+
+    # Now go through both arrays until all the elements
+    # make it into the resultant array
+    while len(result) < len(left) + len(right):
+        # The elements need to be sorted to add them to the
+        # resultant array, so you need to decide whether to get
+        # the next element from the first or the second array
+        if left[index_left] <= right[index_right]:
+            result.append(left[index_left])
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
+
+        # If you reach the end of either array, then you can
+        # add the remaining elements from the other array to
+        # the result and break the loop
+        if index_right == len(right):
+            result += left[index_left:]
+            break
+
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+
+    return result
+
+def merge_sort(array):
+    # If the input array contains fewer than two elements,
+    # then return it as the result of the function
+    if len(array) < 2:
+        return array
+
+    midpoint = len(array) // 2
+
+    # Sort the array by recursively splitting the input
+    # into two equal halves, sorting each half and merging them
+    # together into the final result
+    return merge(
+        left=merge_sort(array[:midpoint]),
+        right=merge_sort(array[midpoint:]))
+
+def quicksort(array):
+    # If the input array contains fewer than two elements,
+    # then return it as the result of the function
+    if len(array) < 2:
+        return array
+
+    low, same, high = [], [], []
+
+    # Select your `pivot` element randomly
+    pivot = array[randint(0, len(array) - 1)]
+
+    for item in array:
+        # Elements that are smaller than the `pivot` go to
+        # the `low` list. Elements that are larger than
+        # `pivot` go to the `high` list. Elements that are
+        # equal to `pivot` go to the `same` list.
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+
+    # The final result combines the sorted `low` list
+    # with the `same` list and the sorted `high` list
+    return quicksort(low) + same + quicksort(high)
+
+def timsort(array):
+    min_run = 32
+    n = len(array)
+
+    # Start by slicing and sorting small portions of the
+    # input array. The size of these slices is defined by
+    # your `min_run` size.
+    for i in range(0, n, min_run):
+        insertion_sort(array, i, min((i + min_run - 1), n - 1))
+
+    # Now you can start merging the sorted slices.
+    # Start from `min_run`, doubling the size on
+    # each iteration until you surpass the length of
+    # the array.
+    size = min_run
+    while size < n:
+        # Determine the arrays that will
+        # be merged together
+        for start in range(0, n, size * 2):
+            # Compute the `midpoint` (where the first array ends
+            # and the second starts) and the `endpoint` (where
+            # the second array ends)
+            midpoint = start + size - 1
+            end = min((start + size * 2 - 1), (n-1))
+
+            # Merge the two subarrays.
+            # The `left` array should go from `start` to
+            # `midpoint + 1`, while the `right` array should
+            # go from `midpoint + 1` to `end + 1`.
+            merged_array = merge(
+                left=array[start:midpoint + 1],
+                right=array[midpoint + 1:end + 1])
+
+            # Finally, put the merged array back into
+            # your array
+            array[start:start + len(merged_array)] = merged_array
+
+        # Each iteration should double the size of your arrays
+        size *= 2
+
+    return array
+
+def run_sorting_algorithm(algorithm, array):
+    # Set up the context and prepare the call to the specified
+    # algorithm using the supplied array. Only import the
+    # algorithm function if it's not the built-in `sorted()`.
+    setup_code = f"from __main__ import {algorithm}" \
+        #if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    # Execute the code ten different times and return the time
+    # in seconds that each execution took
+    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
+
+    # Finally, display the name of the algorithm and the
+    # minimum time it took to run
+    #
+    print(times)
+
+    final_times=[]
+    for time in times:
+        final_times.append(round(time, 6))
+
+    print(f"Algorithm: {algorithm}. Array lenght: {len(array)}. Minimum execution time: {min(final_times)} sec")
+    print("")
+
+ARRAY_LENGTH = 1000
 
 if __name__ == "__main__":
     # Generate an array of `ARRAY_LENGTH` items consisting
     # of random integer values between 0 and 999
     array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
-
+    # array = [i for i in range(ARRAY_LENGTH)]
+ 
     # Call the function using the name of the sorting algorithm
     # and the array you just created
-    run_sorting_algorithm(algorithm="sorted", array=array)
-    run_sorting_algorithm(algorithm="bubble_sort", array=array)
+    #run_sorting_algorithm(algorithm="sorted", array=array)
+    #run_sorting_algorithm(algorithm="bubble_sort", array=array)
+    run_sorting_algorithm(algorithm="insertion_sort", array=array)
+    run_sorting_algorithm(algorithm="merge_sort", array=array)
+    run_sorting_algorithm(algorithm="quicksort", array=array)
+    # run_sorting_algorithm(algorithm="timsort", array=array)
